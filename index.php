d13bfbab03a124069a06cec5d0a3bcac99a3d6a3
Egiave <e.veblauskaite@gmail.com>
2019-04-16 15:48:11 +0300

    date

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Aš valdau HTML</title>
-</head>
-<body>
-    <h1> Vardas - HTML asė!</h1>
-    <p> Moku kurti statinį turinį </p>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Aš valdau HTML <?php print 'ir PHP'; ?></title>
+    </head>
+    <body>
+        <h1>Egita - HTML <?php print 'ir PHP'; ?> asė!</h1>
+        <p>Kuriu dinaminį turinį. Atidaryta:
+        <?php print date('2019-04-16 15:45'); ?>  
+        </p>  
+    </body>
 </html>
