658c12d90719330dfbf197244c97b3f0adab1aa8
Egiave <e.veblauskaite@gmail.com>
2019-04-17 14:55:24 +0300

    variables

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>PHP lydės ir<?php print date (rand(2019, 2029)) . "-" . date(rand(1,12)) . "-" . date(rand(1,31)); ?>
-        </title>
-    </head>
-       <body style="background-color: rgb(<?php print rand(000,255) . ',' . rand(000,255) . ',' . rand(000,255);?>)">
-            <h1>Egita - Galbūt turėsiu <?php print rand (1, 5); ?>              
-              vaikų(us)!<h1> 
-            <p>D. Trump'as nebebus prezidentu<?php print date (rand(2021, 2031)) . date (' M-d'); ?></p>
-            <h1 style="font-size: <?php print rand(1,100) . 'px'; ?>;">Aš keičiu savo dydį</h1>
-            <p style="color: rgb(<?php print rand(000,255) . ',' . rand(000,255) . ',' . rand(000,255);?>)">Aš keičiu savo spalvą</p>   
-            <img src="Dice/<?php print rand(1,6); ?>.png" >
-        </body>
-</html>
+ <?php 
+// STRING
+ $string = 'Hello'; 
+  print "String: $string <br>";
+ $integer = 0;
+  print "Integer: $integer <br>";
+  
+ $float = 1.234;
+ print "Float: $float <br>";
+ $boolean = true;
+ print "Boolean: $boolean <br>";
+ $nothing = null;
+ print "Nothing: $nothing <br>";
 
+ ?>
 
