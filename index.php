7be5888a9229837b54140093f3a8b5538b30fdbb
EV <e.veblauskaite@gmail.com>
2019-04-24 20:13:24 +0300

    04 25 teamwork

--- index.php
+++ index.php
@@ -1,97 +1,11 @@
 <?php
 
-$sunny = 1;
+$background = rand(1, 2);
 
-if ($sunny) {
-    print 'Sauleta';
+if ($background == 1) {
+    $css_class = 'img';
 } else {
-    print 'Debesuota';
-}
-
-$x = 0;
-
-if ($x == 1) {
-    print 'x yra 1';
-} elseif ($x == 2) {
-    print 'x yra 2';
-} else {
-    print 'Nei tas, nei tas';
-}
-
-$distance = rand(800, 10000);
-$consumption = 7.5;
-$price_1 = 1.3;
-
-$fuel_total = round(($distance * 7.5) / 100);
-$price_trip = round($fuel_total * $price_1);
-$text = 'Nuvažiavus ' . $distance . ' km, ' . ' mašina sunaudos ' . $fuel_total . ' l ' . ' kuro. ' . ' Kaina: ' . $price_trip . ' pinigų.';
-
-$my_money = 100;
-If ($my_money >= $price_trip) {
-    $isvada = 'galiu';
-} else {
-    $isvada = 'negaliu';
-}
-
-$text2 = 'Išvada: Aš galiu sau tai ' . $isvada . ' leisti.';
-
-$grizai_velai = rand(0, 1);
-$grizai_isgeres = rand(0, 1);
-
-if ($grizai_velai && $grizai_isgeres) {
-    $atsakymas = 'Miegosi ant sofos, nes grįžai vėlai ir išgėręs.';
-} elseif ($grizai_velai && !$grizai_isgeres) {
-    $atsakymas = 'Nemiegosi ant sofos, nes negrįžai išgėręs.';
-} elseif (!$grizai_velai && $grizai_isgeres) {
-    $atsakymas = 'Nemiegosi ant sofos, nes negrįžai vėlai.';
-} else {
-    $atsakymas = 'Nemiegosi ant sofos, nes nieko nepadarei.';
-}
-
-$oras = rand(0, 1);
-
-if ($oras) {
-    $img1 = 'https://cdn.iconscout.com/icon/free/png-256/brightness-summer-sun-sunny-hot-38953.png';
-    $tekstas = 'Sauleta';
-} else {
-    $img1 = 'https://cdn2.iconfinder.com/data/icons/wthr/32/cloud-512.png';
-    $tekstas = 'Debesuota';
-}
-
-
-$oras = rand(0, 1);
-
-if ($oras) {
-    $css_class = 'sauleta';
-    $tekstas = 'Saulėta';
-} else {
-    $css_class = 'debesuota';
-    $tekstas = 'Debesuota';
-}
-
-$sunny = rand(0, 1);
-$rain = rand(0, 1);
-
-if ($sunny && $rain) {
-    $css_class = 'fas fa-rainbow';
-    $textone = 'Rainbow';
-} elseif ($sunny && !$rain) {
-    $css_class = 'fas fa-sun';
-    $textone = 'Sunny';
-} elseif (!$sunny && $rain) {
-    $css_class = 'fas fa-cloud-showers-heavy';
-    $textone = 'Rain';
-} else {
-    $css_class = 'fas fa-cloud';
-    $textone = 'Cloudly';
-}
-
-if (date('s') % 2 == 0) {
-    $class = 'kvadratas';
-    $text = date('s');
-} else {
-    $class = 'apskritimas';
-    $text = date('s');
+    $css_class = 'img1';
 }
 
 ?>
@@ -99,101 +13,105 @@ if (date('s') % 2 == 0) {
 <html>
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1">
-        <title>Klases darbas</title>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <title>THE best</title>
         <style type="text/css">
-            .rainbow {
-                background-image: url('https://cdn.iconscout.com/icon/free/png-256/brightness-summer-sun-sunny-hot-38953.png');
+            @import url('https://fonts.googleapis.com/css?family=Antic+Slab');
+            
+            .img {
+                background-image: url('https://thumbs.dreamstime.com/z/disco-party-astronauts-dancing-men-women-disco-party-astronauts-dancing-men-women-pop-art-retro-comic-book-vector-cartoon-111010419.jpg');
                 background-size: cover;
-                height: 10%;
-                width: 5%;
+                height: 100vh;
             }
-            .debesuota {
-                background-image: url('https://cdn2.iconfinder.com/data/icons/wthr/32/cloud-512.png');
+            
+            .img1 {
+                background-image: url('http://media.bizj.us/view/img/6982812/prosser*750xx8805-4960-0-1682.jpg');
                 background-size: cover;
-                height: 6%;
-                width: 3%;
+                height: 100vh;
             }
-
-            .sauleta {
-                background-image: url('https://cdn.iconscout.com/icon/free/png-256/brightness-summer-sun-sunny-hot-38953.png');
-                background-size: cover;
-                height: 10%;
-                width: 5%;
-            }
-            .debesuota {
-                background-image: url('https://cdn2.iconfinder.com/data/icons/wthr/32/cloud-512.png');
-                background-size: cover;
-                height: 6%;
-                width: 3%;
-            }
-        </style>
-        <style type="text/css">
-            .kvadratas {
-                height: 150px;
-                width: 150px;
-                background-color: red;
-            }
-            .apskritimas {
-                height: 150px;
-                width: 150px;
-                border-radius: 50%;
-                background-color: blue;
-            }
-            .flex {
+            
+            body {
                 display: flex;
-                flex-direction: row;
-                flex-wrap: nowrap;
-                justify-content: center;
                 align-items: center;
-                align-content: center;
+                justify-content: center;
+                flex-direction: column;
+                font-family: 'Antic Slab', serif;
+            }
+            
+            #about {
+                text-align: center;
+                background-color: rgba(255, 255, 255, 0.8);
+                height: 50%;
+                width: 50%;
                 float: none;
                 margin: auto;
             }
+            
+            .text {
+               color: #142d4c;
+               font-size: 50px;
+            }
+            
+            .textone {
+                color: #142d4c;
+                font-size: 30px;
+                font-weight: bold;
+            }
+            
+            .textbody {
+                color: #142d4c; 
+                list-style: none;
+                font-size: 20px;
+                font-weight: bold;
+            }
+            
+            .thebest {
+                font-size: 50px;
+                font-style: italic;
+            }
+            
+            .thebest1 {
+                font-size: 40px;
+                font-style: italic;
+            }
+            
         </style>
     </head>
-    <body>
-        <?php
-        print $text . '<br>';
-        print $text2;
-        ?>
-
-        <h1><?php print $atsakymas; ?></h1>
-
-        <img style="width: 3%;" src="<?php print $img1; ?>">
-        <?php print $tekstas; ?>
-
-        <div class="container">
-            <div class="<?php print $css_class; ?>"></div>
-            <p><?php print $tekstas; ?></p>
-        </div>
-
-        <div class="container">
-            <h1 class="<?php print $css_class; ?>">
-                <?php print $textone; ?>
-            </h1>
-        </div>
-
-        <div class="container">
-            <div class="<?php print $css_class; ?>">
-                <p class="fas fa-rainbow">
-                    <?php print $textone; ?>
-                </p>
-            </div>
-        </div>
-
-        <div class="container">
-            <div class="<?php print $css_class; ?>">
-                <p class="fas fa-rainbow">
-                    <?php print $textone; ?>
-                </p>
-            </div>
-        </div>
+    
+        <body class="<?php print $css_class; ?>">
         
-        <div class="flex <?php print $class ?>">
-            <p><?php print $text; ?></p>
-        </div>
         
+        <header>    
+            <nav>
+		<a href="#Home">Home</a>
+		<a href="#kiausrakulas">Kiausrakulas</a>
+		<a href=#About>About</a>
+		<a href=#Contact>Contact</a>
+            </nav>	
+        </header>     
+       
+            
+        <div id="about">
+            <h1 class="text">We are <span class='thebest'>THE best!</span></h1>
+                <p class="textone">
+                    <span class='thebest'>THE best</span> success story
+                </p>
+            <ul>
+            <li class="textbody"> 
+                Mes esame <span class='thebest1'>THE best</span> tarp THE BEST!  
+            </li>
+            <li class="textbody">
+                Vakar mes buvome <span class='thebest1'>THE best</span> tarp THE BEST!
+            </li>
+            <li class="textbody">
+                Rytoj mes vis dar bÅ«sime <span class='thebest1'>THE best</span> tarp THE BEST!
+            </li>
+            <li class="textbody">
+                Jeigu dar neprisimenate, mes esame <span class='thebest1'>THE best</span> tarp THE BEST!
+            </li>
+            <li class="textbody">
+                Ar jau Å¾inote, kas yra <span class='thebest1'>THE best?</span>
+            </li>
+            </ul>  
+        </div>
     </body>
 </hmtl>    
