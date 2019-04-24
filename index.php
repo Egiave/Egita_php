5c7006b7eb4de275bc3b71be1e7c2aa10a4c9358
EV <e.veblauskaite@gmail.com>
2019-04-25 00:27:32 +0300

    uzduotis: car_price + santaupos + pinigulikutis.

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
 <?php
 
-$x = 10;
-
-for ($z = 1; $z <= $x; $z++) {
-    print "Aš esu $z-tasis ciklas <br>"; 
-}
-
 $months = 24;
 $car_price_new = 30000;
 $car_price_used = $car_price_new;
+$santaupos = 15000;
 
-for ($m = 1; $m <=24; $m++) {
+for ($m = 1; $santaupos <= $car_price_used; $m++) {
     $car_price_used -= round($car_price_used * 0.02);
     print "$m Mašinos vertė $car_price_used <br>";
+//    if ($santaupos >= $car_price_used) {
+//         print "Po $m mėnesių turėsiu tinkamą pinigų sumą. ";
 }
 
+$pinigulikutis = $santaupos - $car_price_used;
 $depreciated = round(100 - ($car_price_used / $car_price_new) * 100);
-$text = "Po 24 menesiu masinos verte bus: $car_price_used ir nuvertejo $depreciated procentu."
+$text = "Po $m mėnesių mašinos vertė bus: $car_price_used eurų. Nusipirkus tau liks $pinigulikutis eur."
 
 ?>
 <!DOCTYPE html>
