07a1a1a19062eedfb34c18fc24d476e62027c401
EV <e.veblauskaite@gmail.com>
2019-04-26 13:53:14 +0300

    uzduotis: mano_amzius (04.26)

--- about.php
+++ about.php
@@ -1,18 +1,19 @@
 <?php
 
-$mano_atmintis = [
-	    'Penktadienis',
-	    'Paskaita',
-	    'Baras',
-	    'Viskis',
-	    'Alus',
-	    'Degtine',
-	    'Alus',
-	    'Pirmadienis',
-	    'Paskaita'
-	];
+function amzius ($amzius) {
+if ($amzius < 0) {
+    return 'Klaida';
+}    
+if ($amzius < 18) {
+   return 'nepilnametis';
+}  if ($amzius < 65) {
+   return 'pilnametis';  
+}  if ($amzius < 100) {
+   return 'pensininkas'; 
+}  return 'negyvas'; 
+}
 
-$rand_flashback = rand(0, count($mano_atmintis) - 1);
+print amzius(70);
 
 ?>
 <!DOCTYPE html>
@@ -22,18 +23,6 @@ $rand_flashback = rand(0, count($mano_atmintis) - 1);
         <title>Array</title>
     </head>
     <body>
-        <h1>Kas buvo penktadienį?!</h1>
-        <h2>Mano atmintis</h2>
-            <ul> <!-- foreach -->
-                <?php foreach($mano_atmintis as $value): ?>
-                <li> 
-                   <?php print $value; ?>
-                </li>
-                <?php endforeach; ?>
-            </ul><!-- endforeach -->
-                <?php 
-//              print $mano_atmintis["$rand_flashback"];
-                print '#' . $rand_flashback . ': ' . $mano_atmintis[$rand_flashback];
-                ?>
+       
     </body>    
 </html>    
