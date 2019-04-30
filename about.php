40d118f71ca923275622bfb6a2394ad1cc0a6212
EV <e.veblauskaite@gmail.com>
2019-04-30 14:26:41 +0300

    Nuolaida_gerimai

--- about.php
+++ about.php
@@ -1,34 +1,59 @@
 <?php
 
-$zodziai = [
-    'Petras',
-    'laukė',
-    'ryte',
-    'prie',
-    'Maxima'
+$gerimai = [
+    [
+        'name' => 'Vilkmerges alus',
+        'kaina' => 4.50,
+        'nuolaida' => 0
+    ],
+    [
+        'name' => 'Stumbro Degtine',
+        'kaina' => 8.50,
+        'nuolaida' => 10
+    ],
+    [
+        'name' => 'Vynas',
+        'kaina' => 10,
+        'nuolaida' => 2 
+    ],
+    [
+        'name' => 'Zemaitijos pienas',
+        'kaina' => 2,
+        'nuolaida' => 0
+   ],
 ];
-$rasinys = [];
-$rasinio_ilgis = rand(100, 300);
 
-for ($rasinio_zodis = 0; $rasinio_zodis < $rasinio_ilgis; $rasinio_zodis++) {
-    $random_index = rand(0, count($zodziai) - 1);
-    $random_zodis = $zodziai[$random_index];
-    $rasinys[] = $random_zodis;
+foreach ($gerimai as $index => $irasas) {
+    if ($irasas['nuolaida'] > 0) {
+        $gerimai[$index]['css_class'] = 'positive';
+    } else {
+        $gerimai[$index]['css_class'] = 'negative';
+    }
 }
 
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Klases darbas, pirmadienis</title>
+        <title>Klases darbas, antradienis</title>
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="style.css"> 
+         <style type="text/css">
+            .positive {
+                font-size: 20px;
+            }
+            .negative {
+                font-size: 10px;
+            }
+        </style>
+        
     </head>
     <body>
-        <ul>
-            <?php foreach ($rasinys as $zodis): ?>
-                <li>
-                    <?php print $zodis; ?>
+       <ul>
+            <?php foreach ($gerimai as $irasas): ?>
+                <li class="<?php print $irasas['css_class']; ?>">
+                    <span><?php print $irasas['name']; ?></span>
+                    <span><?php print $irasas['nuolaida']; ?></span>
                 </li>
             <?php endforeach; ?>    
         </ul>
