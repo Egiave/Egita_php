d92b171743cdb7d5be9b476b7a5b07324347dd0d
EV <e.veblauskaite@gmail.com>
2019-05-01 19:43:08 +0300

    for loop: $grikiai (3 version)

--- about.php
+++ about.php
@@ -1,25 +1,31 @@
 <?php
 
 $grikiai_start = 5000;
-$praejo_dienu = 0;
+$grikiai = $grikiai_start;
+$survive_days = 0;
+$result = "";
 
-for ($grikiai = $grikiai_start; $grikiai > 0; $grikiai -= rand(200, 500)) {
-    $praejo_dienu++;
-}
-
-$date = date('Y-M-d', strtotime("+$praejo_dienu days"));
-$text = "Jei turesiu $grikiai_start g. grikiu, valgysiu juos $praejo_dienu dienu."
-        . " Jei pradesiu siandien, grikiai baigsis $date data";
+for($day = 1; $day < 100; $day++){
+      $per_diena = rand(200, 500);
+      if ($grikiai >= 500){
+          $grikiai -= $per_diena;
+          $survive_days++;
+          $result .= "Grikiu yra $grikiai per $survive_days dienas." . '<br>';
+      }else{
+          $result .= "Paskutine diena $survive_days" . "grikiu liko $grikiai " . '<br>';
+          break;
+      }
+ }
 
 ?> 
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Klases darbas, antradienis</title>
+        <title>Klases darbas</title>
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="style.css">    
     </head>
     <body>
-        <?php print $text; ?>
+        <?php print $result; ?>
     </body>
 </html>
