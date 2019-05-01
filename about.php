cb903ca0363cffa55d2eff55ce1e4253e6da4698
EV <e.veblauskaite@gmail.com>
2019-05-01 19:24:37 +0300

    for loop: $grikiai sutrumpintas (final)

--- about.php
+++ about.php
@@ -1,32 +1,17 @@
 <?php
 
-$days = 365;
-$pakelio_kaina = 3.5;
-$surukyta = 0;
-$surukyta2 = 0;
-$rukymo_laikas = 5;
+$grikiai_start = 5000;
+$praejo_dienu = 0;
 
-for ($day = 1; $day <= $days; $day++){
-    $weekday = date('N', strtotime("+$day days"));
-    if ($weekday <= 4) {
-        $cizos_mon_thu = rand(3, 4);
-        $surukyta += $cizos_mon_thu;
-        $surukyta2 += $cizos_mon_thu;
-    } elseif ($weekday == 5) {
-        $cizos_fri = rand(10, 20);
-        $surukyta += $cizos_fri;
-    } else {
-        $cizos_sat_sun = rand(1, 3);
-        $surukyta += $cizos_sat_sun;
-    }
-} 
-$viso_hours = round(($surukyta * $rukymo_laikas) / 60);
-$viso_kaina = round($surukyta / 20 * 3.5);
-$text = "Per metus surūkysiu $surukyta cigarečių už $viso_kaina eur ir praleisiu rūkydamas $viso_hours.";
-$mon_thu_kaina = round($surukyta2 / 20 * 3.5);
-$text1 = "Nerūkydamas pirmadienį-ketvirtadienį sutaupyčiau $mon_thu_kaina eur."        
+for ($grikiai = $grikiai_start; $grikiai > 0; $grikiai -= rand(200, 500)) {
+    $praejo_dienu++;
+}
 
-?>
+$date = date('Y-M-d', strtotime("+$praejo_dienu days"));
+$text = "Jei turesiu $grikiai_start g. grikiu, valgysiu juos $praejo_dienu dienu."
+        . " Jei pradesiu siandien, grikiai baigsis $date data";
+
+?> 
 <!DOCTYPE html>
 <html>
     <head>
@@ -35,6 +20,6 @@ $text1 = "Nerūkydamas pirmadienį-ketvirtadienį sutaupyčiau $mon_thu_kaina eu
         <link rel="stylesheet" type="text/css" href="style.css">    
     </head>
     <body>
-        <?php print $text .' '. $text1; ?>
+        <?php print $text; ?>
     </body>
 </html>
