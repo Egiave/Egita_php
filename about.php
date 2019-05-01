e16630db9348d6765c55efce7ede3c2b7d484c30
EV <e.veblauskaite@gmail.com>
2019-05-01 16:55:05 +0300

    For loop: uzduotis - Kisene/alga/islaidos (koreguota)

--- about.php
+++ about.php
@@ -1,63 +1,22 @@
 <?php
 
-function god () {
-    print 'wine';
-}
+//Uzduotis Nr.2
+$months = 12;
+$kisene = 1000;
+$alga = 700;
 
-god();
-
-function px_to_color($lenght){
-    if ($lenght > 30) {
-        print 'Black';
+for ($m = 1; $m <= $months; $m++) {
+    $islaidos = rand(500, 1200);
+    $balansas = $kisene + $alga - $islaidos;
+//    print "$islaidos<br>";
+    print "menuo $m<br>kisene $kisene eur<br>islaidos $islaidos<br>balansas $balansas<br><br>";
+    if ($balansas <= 0) {
+        print "Bloga prognozė: $m mėnesį gali baigtis pinigai! Atsargiai!. Likutis $kisene. <br>";
+        break;
     } else {
-        print 'White';
+        $kisene = $balansas;   
     }
 }
-
-px_to_color(40);
-
-/**
- * sumuoja x ir y
- * 
- * @param integer $x Pirma verte
- * @param integer $y Antra verte
- * @return integer
- */
-
-function sum($x, $y) {
-    return $x + $y;
-} 
-
-$suma = sum(10, 20);
-print sum(10, 20) . '<br>';
-
-function daugyba($x, $y) {
-    return $x * $y;
-}
-print daugyba (2, 5) . '<br>';
-
-/**
- * skaiciuoja vidurki is masyvo
- * @param array $array
- * @return float
- */
-
-function average($array) {
-    $sum = 0;
-    foreach ($array as $value) {
-        $sum += $value;
-    }
-    return $sum / count($array); 
-}
-
-$skaiciai = [12, 43, 654, 23];        
-print average ($skaiciai) . '<br>';
-
-function dalyba ($x, $y) {
-    return $x / $y;
-}
-print dalyba (6, 3) . '<br>';
-
 
 ?>
 <!DOCTYPE html>
@@ -67,6 +26,6 @@ print dalyba (6, 3) . '<br>';
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="style.css">    
     </head>
-    <body> 
+    <body>
     </body>
 </html>
