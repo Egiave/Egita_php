b6edaa243560369ff3e8e11e34b6d651618e3f53
EV <e.veblauskaite@gmail.com>
2019-05-01 18:23:09 +0300

    For loop: uzduotis - $viso_hours rukant

--- about.php
+++ about.php
@@ -4,6 +4,7 @@ $days = 365;
 $pakelio_kaina = 3.5;
 $surukyta = 0;
 $surukyta2 = 0;
+$rukymo_laikas = 5;
 
 for ($day = 1; $day <= $days; $day++){
     $weekday = date('N', strtotime("+$day days"));
@@ -19,9 +20,9 @@ for ($day = 1; $day <= $days; $day++){
         $surukyta += $cizos_sat_sun;
     }
 } 
-
+$viso_hours = round(($surukyta * $rukymo_laikas) / 60);
 $viso_kaina = round($surukyta / 20 * 3.5);
-$text = "Per metus surūkysiu $surukyta cigarečių už $viso_kaina eur.";
+$text = "Per metus surūkysiu $surukyta cigarečių už $viso_kaina eur ir praleisiu rūkydamas $viso_hours.";
 $mon_thu_kaina = round($surukyta2 / 20 * 3.5);
 $text1 = "Nerūkydamas pirmadienį-ketvirtadienį sutaupyčiau $mon_thu_kaina eur."        
 
