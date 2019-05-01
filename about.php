60dca9431231ba8717f7dc19981a95343e01f5a0
EV <e.veblauskaite@gmail.com>
2019-05-01 14:37:43 +0300

    Functions - 04.30

--- about.php
+++ about.php
@@ -1,43 +1,63 @@
 <?php
 
-$gerimai = [
-    [   
-        'name' => 'Vilkmerges alus',
-        'kaina' => 4.50,
-        'nuolaida' => 0
-    ],
-    [   
-        'name' => 'Stumbro Degtine',
-        'kaina' => 8.50,
-        'nuolaida' => 10,
-        'kaina su nuolaida' => 7.65
-    ],
-    [   
-        'name' => 'Vynas',
-        'kaina' => 10,
-        'nuolaida' => 10,
-        'kaina su nuolaida' => 9
-    ],
-    [   
-        'name' => 'Zemaitijos pienas',
-        'kaina' => 2,
-        'nuolaida' => 0
-   ],
-];
-      
-foreach ($gerimai as $index => $gerimas) {
-    if ($gerimas['nuolaida'] > 0) { 
-        $gerimai[$index]['css_class'] = 'akcija';
-        $kaina_su_nuolaida =  $gerimas['kaina'] * (100 - $gerimas['nuolaida']) / 100; 
-        $gerimai[$index]['kaina_su_nuolaida'] = $kaina_su_nuolaida;
+function god () {
+    print 'wine';
+}
+
+god();
+
+function px_to_color($lenght){
+    if ($lenght > 30) {
+        print 'Black';
     } else {
-        $gerimai[$index]['css_class'] = 'be-akcijos';
+        print 'White';
     }
 }
 
-//&$gerimas
-//unset ($gerimas);
-//var_dump($gerimai);
+px_to_color(40);
+
+/**
+ * sumuoja x ir y
+ * 
+ * @param integer $x Pirma verte
+ * @param integer $y Antra verte
+ * @return integer
+ */
+
+function sum($x, $y) {
+    return $x + $y;
+} 
+
+$suma = sum(10, 20);
+print sum(10, 20) . '<br>';
+
+function daugyba($x, $y) {
+    return $x * $y;
+}
+print daugyba (2, 5) . '<br>';
+
+/**
+ * skaiciuoja vidurki is masyvo
+ * @param array $array
+ * @return float
+ */
+
+function average($array) {
+    $sum = 0;
+    foreach ($array as $value) {
+        $sum += $value;
+    }
+    return $sum / count($array); 
+}
+
+$skaiciai = [12, 43, 654, 23];        
+print average ($skaiciai) . '<br>';
+
+function dalyba ($x, $y) {
+    return $x / $y;
+}
+print dalyba (6, 3) . '<br>';
+
 
 ?>
 <!DOCTYPE html>
@@ -45,33 +65,8 @@ foreach ($gerimai as $index => $gerimas) {
     <head>
         <title>Klases darbas, antradienis</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="style.css"> 
-         <style type="text/css">
-             .akcija {
-                font-size: 20px;
-             }
-            .akcija .kaina {
-                text-decoration: line-through;
-            }
-            .be-akcijos {
-                font-size: 10px;
-            }
-            
-            
-        </style>
-        
+        <link rel="stylesheet" type="text/css" href="style.css">    
     </head>
-    <body>
-       <ul>
-            <?php foreach ($gerimai as $gerimas): ?>
-                <li class="<?php print $gerimas['css_class']; ?>">
-                    <span><?php print $gerimas['name']; ?></span>
-                    <?php if (isset($gerimas['kaina_su_nuolaida'])): ?>
-                    <span><?php print $gerimas['kaina_su_nuolaida']; ?></span> 
-                    <?php endif; ?>
-                    <span class="kaina"><?php print $gerimas['kaina']; ?></span>
-                </li>
-            <?php endforeach; ?>    
-        </ul>
+    <body> 
     </body>
 </html>
