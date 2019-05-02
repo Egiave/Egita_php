de4420c9b47ead21c23416c41e2b1f333a905c0e
EV <e.veblauskaite@gmail.com>
2019-05-02 11:12:34 +0300

    functions: 3x3 skirtingu spalvu div

--- about.php
+++ about.php
@@ -1,28 +1,66 @@
 <?php
 
 /**
- * 
- * @return array generuoja 3x3 random array
+ * @return array 3x3 random array
  */
-
-function slot_run (){
+function slot_run() {
     $array = [
-        [rand(0,1), rand(0,1), rand(0,1)],
-        [rand(0,1), rand(0,1), rand(0,1)],
-        [rand(0,1), rand(0,1), rand(0,1)],
+        [rand(0, 1), rand(0, 1), rand(0, 1)],
+        [rand(0, 1), rand(0, 1), rand(0, 1)],
+        [rand(0, 1), rand(0, 1), rand(0, 1)]
     ];
     return $array;
 }
-var_dump (slot_run());
-    
-?> 
+
+$array1 = slot_run();
+
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
-        <title>Klases darbas</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="style.css">    
+        <meta charset="UTF-8">
+        <title>Title</title>
+        <link rel="stylesheet" href="style.css">
+        <style>
+            section div {
+                border: 3px solid black;
+                padding: 20px;
+                margin: 20px;
+                height: 5vh;
+                width: 5vh;
+            }
+
+            section {
+                display: flex;
+                justify-content: center;
+                align-items: baseline;
+            }
+            
+            .blue {
+                background-color: blue;
+            }
+            
+            .orange {
+                background-color: orange;
+            }
+        </style>
     </head>
     <body>
+        <?php foreach ($array1 as $index): ?>
+            <section>
+                <?php foreach ($index as $value): ?>
+                    <?php if ($value > 0): ?>
+                        <div class="blue"></div>
+                    <?php else: ?>
+                        <div class="orange"></div>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            </section>
+        <?php endforeach; ?>
     </body>
 </html>
+
+
+
+
+
