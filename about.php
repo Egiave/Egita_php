2fce486466a5f9469f05abc046296cd64ddfd27a
EV <e.veblauskaite@gmail.com>
2019-05-02 15:39:53 +0300

    functions: 5x5 su for ciklais_size_html

--- about.php
+++ about.php
@@ -1,8 +1,9 @@
 <?php
 
 /**
- * generuoja dinamine matrica
- * @return array matrica
+ * generuoja dinaminę matricą
+ * @param integer $size Matricos dydis
+ * @return array
  */
 function slot_run($size) {
     $array = [];
@@ -20,7 +21,9 @@ function slot_run($size) {
     return $array;
 }
 
-var_dump(slot_run(10));
+//var_dump(slot_run(10));
+$size = rand(1, 10);
+$array = slot_run($size);
 
 ?>
 <!DOCTYPE html>
@@ -29,8 +32,41 @@ var_dump(slot_run(10));
         <title>Klasės darbas, 05.02</title>
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="css/style.css">
+        <style>
+            div {
+                border: 3px solid black;
+                padding: 20px;
+                margin: 20px;
+                height: 5vh;
+                width: 5vh;
+            }
+            section {
+                display: flex;
+                justify-content: center;
+                align-items: baseline;
+            }
+
+            .yellow {
+                background-color: blue;
+            }
+
+            .green {
+                background-color: orange;
+            }   
+        </style>        
     </head>
     <body>
+        <?php foreach ($array as $index): ?>
+            <section>
+                <?php foreach ($index as $random): ?>
+                    <?php if ($random): ?>
+                        <div class="yellow"></div>
+                    <?php else: ?>
+                        <div class="green"></div>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            </section>
+        <?php endforeach; ?>
     </body>
 </html>
 
