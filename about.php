442d02838d2e65c0ead6e80d8f63a900b143643b
EV <e.veblauskaite@gmail.com>
2019-05-02 14:50:58 +0300

    functions: 5x5 su for ciklais

--- about.php
+++ about.php
@@ -3,31 +3,24 @@
 /**
  * @return array 3x3 random array
  */
-function tekstas($text) {
-    if(mb_strlen($text) <= 100) {
-       return $text;
+
+function slot_run() {
+    $array = [];
+
+    for ($x = 1; $x <= 5; $x++) {
+        $row = [];
+
+        for ($z = 1; $z <= 5; $z++) {
+            $row[] = rand(0, 1);
+        }
+
+        $array[] = $row;
     }
-    return mb_strcut($text, 0, 97) . '...';
+
+    return $array;
 }
 
-$text = 'Because of the flexible itinerary it was possible to adapt the sights to the number of expected traffic for festivities in Kaunas. Starting with sad history of the Nazi regime killing about 100000 people from Vilnius and just burying the remains in the Paneriai Forest. With the good weather we continued to Kaunas to see many of its churches, the little fortress walking around most of the time. When we finally got to Trakai Castle I learnt about the way of defending the different parts of the castle, how the islands were connected by bridges and what else to do in Trakai. I highly recommend this tour.';
-print tekstas($text) . '<br>';
-
-function mh($text) {
-   $text = str_replace('sudas', '***', $text);        
-    $text = str_replace('pyp', '***', $text);  
-    print $text . '<br>';
-} 
-
-mh('sudas tas gaidys pyp mane');
-
-
-function replaced($text) {
-   return str_replace('sudas', '***', $text);           
-} 
- 
-$text = 'sudas tas gaidys sudas mane';
-print replaced($text);
+var_dump(slot_run());
 
 ?>
 <!DOCTYPE html>
